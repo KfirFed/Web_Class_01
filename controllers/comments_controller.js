2b2397b715f5fc4b5a884cfcc69f1b714c5dfea2
KfirFed <xd.KFederovsky@idf.il>
2024-11-23 21:56:02 +0200

    feat/comments-controller

---
+++ controllers/comments_controller.js
@@ -0,0 +1,41 @@
+const commentsModel = require("../models/comments");
+
+const getCommentsByPostId = async (req, res) => {
+  const postId = req.params.id;
+  try {
+    const comments = await commentsModel.find({ postId: postId });
+    if (comments) {
+      res.send(comments);
+    } else {
+      res.status(404).send("Post not found");
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const getCommentById = async (req, res) => {
+  const commentId = req.params.id;
+  try {
+    const comment = await commentsModel.findById(commentId);
+    if (comment) {
+      res.send(comment);
+    } else {
+      res.status(404).send("Comment not found");
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const createComment = async (req, res) => {
+  const commentBody = req.body;
+  try {
+    const comment = await commentsModel.create(commentBody);
+    res.status(201).send(comment);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+module.exports = { getCommentsByPostId, getCommentById, createComment };
