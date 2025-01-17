b02450a98010592daaef1846c10674d83f601b4b
Ofri Yaakov <ofriyaakov@gmail.com>
2025-01-17 18:02:34 +0200

    add username to register

--- controllers/auth_controller.ts
+++ controllers/auth_controller.ts
@@ -9,6 +9,7 @@ const register = async (req: Request, res: Response) => {
     const hashedPassword = await bcrypt.hash(password, salt);
     const user = await userModel.create({
       email: req.body.email,
+      username: req.body.username,
       password: hashedPassword,
     });
     res.status(200).send(user);
