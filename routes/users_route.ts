6f1986aa3adac4776a6a6607c619e5c3e9603d36
Ofri Yaakov <ofriyaakov@gmail.com>
2025-01-18 00:13:57 +0200

    add swagger to user schema

--- routes/users_route.ts
+++ routes/users_route.ts
@@ -1,6 +1,67 @@
 import express from "express";
-const router = express.Router();
 import usersController from "../controllers/users_controller";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: The Users API
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *       User:
+ *           type: object
+ *           properties:
+ *               _id:
+ *                   type: string
+ *               username:
+ *                   type: string
+ *               password:
+ *                   type: string
+ *               email:
+ *                   type: string
+ *           example:
+ *              _id: '678a7e5148e49d8a24adf3b3'
+ *              username: 'example username'
+ *              password: 'example password'
+ *              email: 'example email@gmail.com'
+ *       CreateUserBody:
+ *           type: object
+ *           required:
+ *              - username
+ *              - password
+ *              - email
+ *           properties:
+ *               username:
+ *                   type: string
+ *               password:
+ *                   type: string
+ *               email:
+ *                   type: string
+ *           example:
+ *              username: 'Ofri'
+ *              password: 'password123'
+ *              email: 'ofri@gmail.com'
+ *       UpdateUserBody:
+ *           type: object
+ *           required:
+ *           properties:
+ *               username:
+ *                   type: string
+ *               password:
+ *                   type: string
+ *               email:
+ *                   type: string
+ *           example:
+ *              username: 'Ofri'
+ *              password: 'new password'
+ *              email: 'ofri@gmail.com'
+ */
 
 router.get("/all", usersController.getAllUsers);
 
